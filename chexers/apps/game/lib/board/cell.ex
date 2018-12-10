a9f3eff721ef5bd6ce581c9a21a96087dd0cf34b
Zach Banducci <zbducci@gmail.com>
2018-12-10 14:10:12 -0600

    Fixed some errors in the cell

--- chexers/apps/game/lib/board/cell.ex
+++ chexers/apps/game/lib/board/cell.ex
@@ -4,8 +4,8 @@ defmodule Game.Board.Cell do
   # defstruct utilizing typedstuct system ni order to prevent
   # invalid information being inserted into cell
   typedstruct enforce: true do
-    field: :row, non_neg_integer()
-    field: :column, non_neg_integer()
-    field: :occupier, :black | :white | nil, default: nil
+    field :row, 0..8
+    field :column, 0..8
+    field :occupier, :black | :white | nil, default: nil
   end
 end
