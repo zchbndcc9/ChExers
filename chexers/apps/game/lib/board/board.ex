f44bd88dc968809e3d5c8afd572183af8b62064b
Zach Banducci <zbducci@gmail.com>
2018-12-11 14:39:17 -0800

    Renamed create_col function & added fxn to determine if cell movable

--- chexers/apps/game/lib/board/board.ex
+++ chexers/apps/game/lib/board/board.ex
@@ -12,21 +12,32 @@ defmodule Game.Board do
   @spec create() :: Game.Board.t()
   def create() do
     []
-    |> create_cells(8, 8)
+    |> create_cells(11, 11)
     |> (&(%Board{ cells: &1 })).()
   end
 
-  defp create_cols(cells, _row, -1) do
+  defp create_row(cells, _row, -1) do
     cells
   end
 
-  defp create_cols(cells, row, col) do
-    create_cols([cells | [%Cell{row: row, col: col}]], row, col-1)
+  defp create_row(cells, row, col) do
+    [cells | [%Cell{row: row, col: col, movable?: movable?(row, col)}]]
+    |> create_row(row, col-1)
   end
 
+  def movable?(row, col) do
+    case {rem(row, 2), rem(col, 2)} do
+      {0, 0} -> true
+      {1, 1} -> true
+      {_, _} -> false
+    end
+  end
+
+  # Creates cells asyncronously
   defp create_cells(cells, row, col) do
     0..row
-    |> Enum.map(fn row -> create_cols(cells, row, col) end)
+    |> Enum.map(fn row -> Task.async(fn -> create_row(cells, row, col) end) end)
+    |> Enum.map(fn task -> Task.await(task) end)
     |> List.flatten
   end
 end
---
+++ chexers/apps/game/test/board/cell_test.exs
@@ -0,0 +1,14 @@
+defmodule Game.Board.CellTest do
+  use ExUnit.Case
+  alias Game.Board.Cell
+
+  describe "Cell creation when" do
+    test "only row and column provided" do
+      cell = %Cell{}
+    end
+
+    test "all fields populated" do
+
+    end
+  end
+end
