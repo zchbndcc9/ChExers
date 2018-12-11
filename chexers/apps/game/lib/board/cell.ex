b062076092d5c9e0993f9e00df447708c0a83545
Zach Banducci <zbducci@gmail.com>
2018-12-11 14:38:04 -0800

    Added movable flag for a cell

--- chexers/apps/game/lib/board/cell.ex
+++ chexers/apps/game/lib/board/cell.ex
@@ -1,11 +1,12 @@
 defmodule Game.Board.Cell do
   use TypedStruct
 
-  # defstruct utilizing typedstuct system ni order to prevent
+  # utilizing typedstuct system in order to prevent
   # invalid information being inserted into cell
   typedstruct enforce: true do
     field :row, 0..8
-    field :column, 0..8
+    field :col, 0..8
+    field :movable?, boolean(), default: false
     field :occupier, :black | :white | nil, enforce: false, default: nil
   end
 end
