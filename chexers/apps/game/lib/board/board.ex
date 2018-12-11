1d44434676585b3c3273dd787324e75b18eda213
Zach Banducci <zbducci@gmail.com>
2018-12-11 15:54:37 -0800

    Consolodated conditions and changed board size

--- chexers/apps/game/lib/board/board.ex
+++ chexers/apps/game/lib/board/board.ex
@@ -1,10 +1,12 @@
 defmodule Game.Board do
   alias Game.Board.Cell
 
-  @spec create() :: list(Cell)
-  def create() do
-    []
-    |> create_cells(11, 11)
+  @spec create(integer()) :: list(Cell)
+  def create(size \\ 8) do
+    0..size-1
+    |> Enum.map(fn row -> Task.async(fn -> create_row([], row, size) end) end)
+    |> Enum.map(fn task -> Task.await(task) end)
+    |> List.flatten
   end
 
   defp create_row(cells, _row, -1) do
@@ -12,23 +14,14 @@ defmodule Game.Board do
   end
 
   defp create_row(cells, row, col) do
-    [cells | [%Cell{row: row, col: col, movable?: movable?(row, col)}]]
+    [%Cell{row: row, col: col, movable?: movable?(row, col)} | cells]
     |> create_row(row, col-1)
   end
 
   def movable?(row, col) do
     case {rem(row, 2), rem(col, 2)} do
-      {0, 0} -> true
-      {1, 1} -> true
+      {same, same} -> true
       {_, _} -> false
     end
-  end
-
-  # Creates cells asyncronously
-  defp create_cells(cells, row, col) do
-    0..row
-    |> Enum.map(fn row -> Task.async(fn -> create_row(cells, row, col) end) end)
-    |> Enum.map(fn task -> Task.await(task) end)
-    |> List.flatten
   end
 end
