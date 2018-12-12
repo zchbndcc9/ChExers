bf0f4a39d814390cc69f1b758607f7ebed1b0296
Zach Banducci <zbducci@gmail.com>
2018-12-11 17:57:13 -0800

    Added conditions to check to see if player moving has won

--- chexers/apps/game/lib/game.ex
+++ chexers/apps/game/lib/game.ex
@@ -9,16 +9,44 @@ defmodule Game do
     field :white_pieces, non_neg_integer, default: 12
     field :black_pieces, non_neg_integer, default: 12
     field :winner, :white | :black | nil, default: nil
+    field :status, atom(), default: :initializing
     field :current_turn, :white | :black | nil, default: nil
   end
 
-  def create() do
-    board = Board.create()
-    |> initialize()
+  def new_game() do
+    board = Board.create() |> initialize()
 
     %Game{ board: board }
   end
 
+  # Guard to ensure that game is not altered once it has already been won
+  def move(_player, game = %Game{status: :won}) do
+    game
+  end
+
+  def move(player, game = %Game{}) do
+    game
+    |> move_piece(player)
+    |> determine_game_status()
+  end
+
+  defp move_piece(game, player) do
+    game
+  end
+
+  # I was torn on whether to pattern match or use cases, but I went with the
+  # latter since I think it leads to clean, more concise code
+  defp determine_game_status(game) do
+    case game do
+      %Game{ white_pieces: 0 } -> %Game{ game | winner: :black, status: :won}
+      %Game{ black_pieces: 0 } -> %Game{ game | winner: :white, status: :won}
+      _ -> %Game{ game | status: :in_progress }
+    end
+  end
+  
+  ## Helpers
+
+  # Adds player pieces to board asynchronously
   defp initialize(board) do
     board = board
     |> Enum.chunk_by(fn %Cell{row: row} -> row end)
@@ -30,7 +58,7 @@ defmodule Game do
     new_board = [Task.await(white_placement) | Task.await(black_placement)]
     new_board = [rest | new_board] |> List.flatten()
 
-    new_board 
+    new_board
   end
 
   defp place_player_pieces(board, start, count, piece) do
