c1d173259ed3911f4ff139671db6f7f104c1f961
Zach Banducci <zbducci@gmail.com>
2018-12-15 14:50:25 -0800

    Added functionality to Board Move module to move and remove pieces

--- chexers/apps/game/lib/board/move.ex
+++ chexers/apps/game/lib/board/move.ex
@@ -1,4 +1,6 @@
 defmodule Game.Board.Move do
+  alias Game.Board.Cell
+
   def move(board, player, from, to) do
     from_cell_task = Task.async(fn -> Enum.find(board, fn cell -> cell.row === from.row and cell.col === from.col end) end)
     to_cell_task = Task.async(fn -> Enum.find(board, fn cell -> cell.row === to.row and cell.col === to.col end) end)
@@ -14,5 +16,11 @@ defmodule Game.Board.Move do
 
     [%Cell{ from_cell | occupier: nil} | [%Cell{to_cell | occupier: player} | filtered_board]]
     |> List.flatten
+  end
+
+  def remove(board, piece_to_remove) do
+    board
+      |> Enum.reject(fn cell -> Map.equal?(cell, piece_to_remove) end)
+      |> (fn board -> [%Cell{piece_to_remove | occupier: nil} | board] end).()
   end
 end
