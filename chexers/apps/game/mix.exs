c5d511d5e07f98d31063412c443235363f46b995
Zach Banducci <zbducci@gmail.com>
2018-12-16 15:50:49 -0800

    Added player dep to game application

--- chexers/apps/game/mix.exs
+++ chexers/apps/game/mix.exs
@@ -5,6 +5,10 @@ defmodule Game.MixProject do
     [
       app: :game,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -19,7 +23,8 @@ defmodule Game.MixProject do
 
   defp deps do
     [
-      {:typed_struct, "~> 0.1.4"}
+      {:typed_struct, "~> 0.1.4"},
+      {:player, in_umbrella: true}
     ]
   end
 end
