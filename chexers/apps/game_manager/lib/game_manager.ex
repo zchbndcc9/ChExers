b78462c139d275d8bdd576b24f1d115f870b5b83
Zach Banducci <zbducci@gmail.com>
2018-12-17 20:06:10 -0800

    Added game controller along with process checkting to game manager

--- chexers/apps/game_manager/lib/game_manager.ex
+++ chexers/apps/game_manager/lib/game_manager.ex
@@ -1,5 +1,6 @@
 defmodule GameManager do
-  defdelegate create_game(),                to: GameManager.GamesSupervisor
-  defdelegate delete_game(name),            to: GaManager.GamesSupervisor
+  defdelegate create_game(),                      to: GameManager.GamesSupervisor
+  defdelegate delete_game(name),                  to: GameManager.GamesSupervisor
   defdelegate move(game_name, player, from, to),  to: GameManager.Games
+  defdelegate get_game(name),                     to: GameManager.Games
 end
---
+++ chexers/apps/server/lib/server_web/controllers/game.controller.ex
@@ -0,0 +1,34 @@
+defmodule ServerWeb.GameController do
+  use ServerWeb, :controller
+
+  def create(conn, _params) do
+    {_status, game_name} = GameManager.create_game()
+
+    conn
+    |> send_resp(200, "{\"game\" => \"#{game_name}\"}")
+  end
+
+  def show(conn, %{"id" => game_id}) do
+    {status, body} =
+      case GameManager.get_game(game_id) do
+        {:ok, game} -> {200, game}
+        {:error, _} -> {400, "No game"}
+      end
+
+      case status do
+        400 ->
+          conn
+          |> send_resp(400, body)
+        200 ->
+          conn
+          |> render(ServerWeb.GameView, "show.json", %{"game" => body})
+      end
+  end
+
+  def delete(conn, %{"id" => game_id}) do
+    {_status, _message} = GameManager.delete_game(game_id)
+
+    conn
+    |> send_resp(200, "Game deleted")
+  end
+end
